14f39bc80ceea3c98d5cc7df074571b87abe4182
Sam O'Cain <samocain93@gmail.com>
2023-01-30 13:46:14 -0600

    init server

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
+const router = require('express').Router(); // require express router module
+const apiRoutes = require('./api'); // bring in api routes
 
-router.use('/api', apiRoutes);
+router.use('/api', apiRoutes); // setting '/api' path as the main route
 
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+  res.send("<h1>Wrong Route!</h1>") // sending back error for the incorrect route
 });
 
 module.exports = router;
--- seeds/index.js
+++ seeds/index.js
@@ -1,10 +1,14 @@
+// Requires for the seed data we're going to be using in the index file
+
 const seedCategories = require('./category-seeds');
 const seedProducts = require('./product-seeds');
 const seedTags = require('./tag-seeds');
 const seedProductTags = require('./product-tag-seeds');
 
+// Requiring the sequilize connection 
 const sequelize = require('../config/connection');
 
+// function to seed the database with the seed files data using async/await and logging to the console when seeded successfully
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
@@ -20,7 +24,9 @@ const seedAll = async () => {
   await seedProductTags();
   console.log('\n----- PRODUCT TAGS SEEDED -----\n');
 
+  // exiting out of process once completed
   process.exit(0);
 };
 
+// inititalizing seed function
 seedAll();
